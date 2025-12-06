0d519820678a1583daa99a6028c580868d57ea8b
Skrybykin Newrgun <SkrybykinNewrgun@gmail.com>
2025-12-06 15:49:05 +0900

    lesson 6

--- Characters/Mobs/skeleton.gd
+++ Characters/Mobs/skeleton.gd
@@ -1,35 +1,88 @@
 extends CharacterBody2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-var chase = false
 var speed = 100
+var is_chase = false
+var is_attack = false
+var is_alive = true
+var player_in_attack_area = false
+var attack_started = false
+var health = 100
+
 @onready var anim = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	# Gravity
+func _physics_process(delta):
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
 	var player = $"../../Player/Player"
-	var direction = (player.position - self.position).normalized()
+	var dir = (player.position - self.position).normalized()
 
-	if chase:
-		velocity.x = direction.x * speed
+	if not is_alive:
+		return
+		
+	if is_attack or attack_started:
+		attack_started = true
+		anim.play("attack")
+		await anim.animation_finished
+		attack_started = false
+		if player_in_attack_area and is_alive:
+			player.apply_damage(20)
+		velocity.x = 0
+		move_and_slide()
+		return
+
+	if is_chase:
+		velocity.x = dir.x * speed
 		anim.play("run")
-		anim.flip_h = direction.x < 0
+		anim.flip_h = dir.x < 0
 	else:
 		velocity.x = 0
 		anim.play("idle")
-	
+
 	move_and_slide()
 
-
-func _on_detector_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+# ====== Триггер погони ======
+func _on_detector_body_entered(body):
 	if body.name == "Player":
-		chase = true
+		is_chase = true
 
-func _on_detector_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_detector_body_exited(body):
 	if body.name == "Player":
-		chase = false
+		is_chase = false
+
+# ====== Триггер атаки ======
+func _on_detector_attack_body_entered(body):
+	if body.name == "Player":
+		is_attack = true
+
+func _on_detector_attack_body_exited(body):
+	if body.name == "Player":
+		is_attack = false
+
+# ====== Нанесение урона ======
+func _on_attack_area_body_entered(body):
+	if body.name == "Player":
+		player_in_attack_area = true
+
+func apply_damage(damage: int):
+	health -= damage
+	if health <1:
+		die()
+
+func die():
+	is_alive = false
+	anim.play("death")
+	await anim.animation_finished
+	queue_free()
+
+
+func _on_dearh_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.velocity.y -= 200
+		die()
+
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		player_in_attack_area = false
--- Characters/player/player.gd
+++ Characters/player/player.gd
@@ -1,57 +1,139 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const INVUL_TIME = 0.7
+const KNOCKBACK_FORCE = 0
 
 @onready var anim = $AnimatedSprite2D
 
+var health = 100
+var is_alive = true
+var is_hurt = false
+var is_attacking = false
+var enemies_in_attack_area = []
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
+	if not is_alive:
+		return
+	
+	if is_attacking:
+		move_and_slide()
+		return
+		
+	if not is_hurt:
+		if Input.is_action_just_pressed("Attack"):
+			perform_attack()
+			return
+		player_movement(delta)
+
+	move_and_slide()
+
+
+func player_movement(delta):
 	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Horizontal movement
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Horizontal
+	var dir = Input.get_axis("MoveLeft", "MoveRight")
 
-	if direction != 0:
-		velocity.x = direction * SPEED
-		anim.flip_h = direction < 0
+	
+	if dir != 0:
+		velocity.x = dir * SPEED
+		anim.flip_h = dir < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Move
-	move_and_slide()
-
-	# Animation logic
-	update_animation(direction)
+	update_animation(dir)
 
 
-func update_animation(direction: float) -> void:
-	# Jump has priority
+func perform_attack():
+	is_attacking = true
+	velocity.x = 0  # игрок не двигается во время атаки
+
+	anim.play("attack")
+
+	# ждём конца анимации атаки
+	await anim.animation_finished
+	deal_damage()
+	is_attacking = false
+
+func deal_damage():
+	for enemy in enemies_in_attack_area:
+		if enemy and enemy.has_method("apply_damage"):
+			enemy.apply_damage(100)
+
+
+func update_animation(dir):
+	if is_hurt:
+		play_anim("take_hit")
+		return
+
 	if velocity.y < 0 and not is_on_floor():
 		play_anim("jump")
 		return
 
-	# Fall second priority
 	if velocity.y > 0 and not is_on_floor():
 		play_anim("fall")
 		return
 
-	# Run
-	if direction != 0:
+	if dir != 0:
 		play_anim("run")
 		return
-
-	# Idle
+	
+	if is_attacking:
+		return
+	
 	play_anim("idle")
 
 
-func play_anim(anim_name: String) -> void:
-	# Чтобы не перезапускать анимацию каждый кадр
-	if anim.animation != anim_name:
-		anim.play(anim_name)
+func play_anim(name_anim):
+	if anim.animation != name_anim:
+		anim.play(name_anim)
+
+
+# ======== ТУТ МАГИЯ HOLLOW KNIGHT =========
+
+func apply_damage(amount: int):
+	if is_hurt or not is_alive:
+		return
+	
+	health -= amount
+	
+	if health <= 0:
+		die()
+		return
+	
+	start_hurt()
+
+
+func start_hurt():
+	is_hurt = true
+	play_anim("take_hit")
+	await anim.animation_finished
+	is_hurt = false
+
+
+func die():
+	is_alive = false
+	velocity = Vector2.ZERO
+	anim.play("death")
+	await anim.animation_finished
+	queue_free()
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		return
+	if not enemies_in_attack_area.has(body):
+			enemies_in_attack_area.append(body)
+
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	if enemies_in_attack_area.has(body):
+		enemies_in_attack_area.erase(body)
